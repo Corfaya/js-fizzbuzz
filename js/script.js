18f6bb3eb80af3beddba1b5f7db362590604b026
Corfaya <lauracorrado0696@gmail.com>
2024-06-27 14:42:13 +0200

    [UPD] Conditional statement scheme added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+// FOR STATEMENT
 for (let i = 0; i <= 100; i++) {
-    
+    // CONDITIONAL STATEMENT
+    // if i is divisible by 3..., else if i is divisible by 5..., else if i is divisible by both 3 and 5..., else...
+    if (i % 3 == 0) {
+
+    } else if (i % 5 == 0) {
+
+    } else if (i % 3 == 0 && i % 5 == 0) {
+
+    } else {
+
+    }
 }
